561a991dcf1f538d16d78ac1d58f395c12d74681
Bartlomiej Kawecki <kawec11@gmail.com>
2021-04-08 07:38:07 +0200

    Weather setup app initial commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Search for Weather</h1>
+      <input type="text" placeholder="City name..." />
+      
+    </div>
+  )
+}
+
+export default Home
