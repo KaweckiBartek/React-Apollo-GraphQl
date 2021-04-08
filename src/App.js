d7946c8ac43ffcb3396c647bb48ef61207eb0b92
Bartlomiej Kawecki <kawec11@gmail.com>
2021-04-08 08:11:19 +0200

    appollo graphql weatcher api works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Home from './Pages/Home';
+import {ApolloClient, InMemoryCache, ApolloProvider, HttpLink} from '@apollo/client'
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'https://graphql-weather-api.herokuapp.com',
+  })
   return (
-    <div className="App">
+    <ApolloProvider {...{client}}>
       <Home />
-    </div>
+
+    </ApolloProvider>
   );
 }
 
